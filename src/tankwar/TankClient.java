478a17f34ce88ea81ecd63f0d4ea09d8a390aac0
刘瑶 <1094131083@qq.com>
2016-06-01 23:40:52 +0800

    2

--- src/tankwar/TankClient.java
+++ src/tankwar/TankClient.java
@@ -1,13 +1,26 @@
 package tankwar;
 
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 
 public class TankClient extends Frame {
-		
+	//继承方法更灵活，可以设置自己的变量和方法；	
 		public void lauchFrame() {
-			this.setLocation(400, 300);
-			this.setSize(800, 600);
-			setVisible(true);
+			this.setLocation(400, 300);//定义窗口出现的位置，以屏幕的左上角为准；
+			this.setSize(800, 600);//设置宽度和高度；
+			this.setTitle("TankWar");
+			//建立一个匿名类来设置窗口关闭的监听；
+			this.addWindowListener(new WindowAdapter(){
+				@Override
+				public void windowClosing(WindowEvent e){
+					System.exit(0);//窗口正常退出；;
+				}
+				
+			});
+			setResizable(false);
+			setVisible(true);// 根据参数 b 的值显示或隐藏此 Window；
 		}
 
 		public static void main(String[] args) {
