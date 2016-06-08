15d45dad981acf9ecc7e5d22f19e235d7a8c60a4
刘瑶 <1094131083@qq.com>
2016-06-08 14:34:50 +0800

    8
    使坦克朝八个方向走；

--- src/tankwar/Tank.java
+++ src/tankwar/Tank.java
@@ -5,8 +5,14 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
 public class Tank {
-	int x,y;
+	private int x,y;
+	public static final int XSPEED=5;
+	public static final int YSPEED=5;
 	
+	private boolean bL=false, bU=false,bR=false,bD=false;
+	//定义枚举类型；
+	enum Direction{L,U,LU,R,RU,D,RD,LD,STOP};
+	Direction dir=Direction.STOP;//坦克默认停止；
 	public Tank(int x,int y){
 		this.x=x;
 		this.y=y;
@@ -17,23 +23,83 @@ public class Tank {
 		//画一个实心圆，参数分别为 x,y,w,h(以x,y为左上角，长宽分别为w,h的内切圆)；
 		g.fillOval(x, y, 30, 30);
 		g.setColor(c);//把画笔设置成原来的颜色；
+		move();
+   }
+   public void move(){
+	   switch(dir){
+	   case L:
+		   x-=XSPEED;
+		   break;
+      case LU:
+    	  x-=XSPEED;
+    	  y-=YSPEED;
+    	   break;
+     case U:
+       y-=YSPEED;
+	   break;
+case RU:
+	  x+=XSPEED;
+	  y-=YSPEED;
+	   break;
+case R:
+	 x+=XSPEED;
+	   break;
+case RD:
+	  x+=XSPEED;
+	  y+=YSPEED;
+	   break;
+case D:
+	  y+=YSPEED;
+	   break;
+case LD:
+	 x-=XSPEED;
+	  y+=YSPEED;
+	   break;
+case STOP:
+	   break;
+	   }
    }
    public void keyPressed(KeyEvent e){
 	   int key=e.getKeyCode();//从键盘获得虚拟的键（返回与此事件中的键关联的整数 keyCode）；
 		//将获得的键和KeyEvent中的常量进行比较；
 		switch(key){
 		case KeyEvent.VK_RIGHT:
-			x+=5;
+			bR=true;
 			break;
 		case KeyEvent.VK_LEFT:
-			x-=5;
+			bL=true;
 			break;
 		case KeyEvent.VK_UP:
-			y-=5;
+			bU=true;
 			break;
 		case KeyEvent.VK_DOWN:
-			y+=5;
+			bD=true;
 			break;
 		}
+		locateDirection();
+   }
+   /**
+    * 设置坦克的方向；
+    */
+   public void locateDirection(){
+	   //按下朝左的键；
+	   if(bL&&!bU&&!bR&&!bD)
+		   dir=Direction.L;
+	   else  if(bL&&bU&&!bR&&!bD)
+		   dir=Direction.LU;
+	   else  if(!bL&&bU&&!bR&&!bD)
+		   dir=Direction.U;
+	   else  if(!bL&&!bU&&bR&&!bD)
+		   dir=Direction.R;
+	   else  if(!bL&&bU&&bR&&!bD)
+		   dir=Direction.RU;
+	   else  if(!bL&&!bU&&!bR&&bD)
+		   dir=Direction.D;
+	   else  if(!bL&&!bU&&bR&&bD)
+		   dir=Direction.RD;
+	   else  if(bL&&!bU&&!bR&&bD)
+		   dir=Direction.LD;
+	   else  if(!bL&&!bU&&!bR&&!bD)
+		   dir=Direction.STOP;
    }
 }
